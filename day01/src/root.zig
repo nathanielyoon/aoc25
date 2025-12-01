76660f8f7e196ffa87eb8b029bae54114ad2e743
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-12-01 13:12:26 -0500

    new: complete 01.1

--- day01/src/root.zig
+++ day01/src/root.zig
@@ -1,23 +1,41 @@
-//! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
 
-pub fn bufferedPrint() !void {
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try stdout.flush(); // Don't forget to flush!
+pub fn solve1(input: []const u8) u32 {
+    var reverse: bool = undefined;
+    var distance: i32 = 0;
+    var state: i32 = 50;
+    var count: u32 = 0;
+    for (input) |character| {
+        switch (character) {
+            'L' => reverse = true,
+            'R' => reverse = false,
+            '0'...'9' => {
+                distance *= 10;
+                distance += character - '0';
+            },
+            '\n' => {
+                if (reverse) distance *= -1;
+                state = @mod(state + distance, 100);
+                if (state == 0) count += 1;
+                distance = 0;
+            },
+            else => unreachable,
+        }
+    }
+    return count;
 }
-
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
+test "example 1" {
+    const example =
+        \\L68
+        \\L30
+        \\R48
+        \\L5
+        \\R60
+        \\L55
+        \\L1
+        \\L99
+        \\R14
+        \\L82
+    ;
+    try std.testing.expectEqual(solve1(example), 3);
 }
