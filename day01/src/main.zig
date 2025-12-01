686640b12752645a8486377a321d0199872ab3c9
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-12-01 17:01:37 -0500

    new: complete 01.2

--- day01/src/main.zig
+++ day01/src/main.zig
@@ -3,7 +3,7 @@ const day01 = @import("day01");
 
 pub fn main() !void {
     const input = @embedFile("./input.txt");
-    const solution = day01.solve1(input);
+    const solution = day01.solve2(input);
 
     var writer = std.fs.File.stdout().writer(&.{});
     try writer.interface.print("{d}\n", .{solution});
--- day01/src/root.zig
+++ day01/src/root.zig
@@ -44,3 +44,47 @@ pub fn solve1(input: []const u8) u32 {
 test "part 1" {
     try std.testing.expectEqual(3, solve1(example));
 }
+
+pub fn solve2(input: []const u8) u32 {
+    var direction: u8 = undefined;
+    var distance: u32 = 0;
+    var state: i32 = 50;
+    var count: u32 = 0;
+    for (input) |character| {
+        switch (character) {
+            'L', 'R' => direction = character,
+            '0'...'9' => {
+                distance *= 10;
+                distance += character - '0';
+            },
+            '\n' => {
+                while (distance > 100) : (distance -= 100) count += 1;
+                switch (direction) {
+                    'L' => {
+                        const prev = state;
+                        state -= @intCast(distance);
+                        if (state < 0) {
+                            state += 100;
+                            if (prev != 0) count += 1;
+                        } else if (state == 0) count += 1;
+                    },
+                    'R' => {
+                        state += @intCast(distance);
+                        if (state >= 100) {
+                            state -= 100;
+                            count += 1;
+                        } else if (state == 0) count += 1;
+                    },
+                    else => unreachable,
+                }
+                direction = undefined;
+                distance = 0;
+            },
+            else => unreachable,
+        }
+    }
+    return count;
+}
+test "part 2" {
+    try std.testing.expectEqual(6, solve2(example));
+}
