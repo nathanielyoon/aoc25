591ea7e5e91dceafdcabddceab01a647ea64cf16
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-12-01 14:24:56 -0500

    fix: clean up a little

--- day01/src/root.zig
+++ day01/src/root.zig
@@ -1,22 +1,39 @@
 const std = @import("std");
+const example =
+    \\L68
+    \\L30
+    \\R48
+    \\L5
+    \\R60
+    \\L55
+    \\L1
+    \\L99
+    \\R14
+    \\L82
+    \\
+;
 
 pub fn solve1(input: []const u8) u32 {
-    var reverse: bool = undefined;
-    var distance: i32 = 0;
+    var direction: u8 = undefined;
+    var distance: u32 = 0;
     var state: i32 = 50;
     var count: u32 = 0;
     for (input) |character| {
         switch (character) {
-            'L' => reverse = true,
-            'R' => reverse = false,
+            'L', 'R' => direction = character,
             '0'...'9' => {
                 distance *= 10;
                 distance += character - '0';
             },
             '\n' => {
-                if (reverse) distance *= -1;
-                state = @mod(state + distance, 100);
+                switch (direction) {
+                    'L' => state -= @intCast(distance),
+                    'R' => state += @intCast(distance),
+                    else => unreachable,
+                }
+                state = @mod(state, 100);
                 if (state == 0) count += 1;
+                direction = undefined;
                 distance = 0;
             },
             else => unreachable,
@@ -24,18 +41,6 @@ pub fn solve1(input: []const u8) u32 {
     }
     return count;
 }
-test "example 1" {
-    const example =
-        \\L68
-        \\L30
-        \\R48
-        \\L5
-        \\R60
-        \\L55
-        \\L1
-        \\L99
-        \\R14
-        \\L82
-    ;
-    try std.testing.expectEqual(solve1(example), 3);
+test "part 1" {
+    try std.testing.expectEqual(3, solve1(example));
 }
