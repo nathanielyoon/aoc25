4c92aae9ad216f19ca7d1852953ad9ecbe74eab9
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-12-03 13:43:12 -0500

    new: complete 03.2

--- 3.zig
+++ 3.zig
@@ -1,36 +1,54 @@
 const std = @import("std");
 
-fn solve(comptime find: fn (bank: []const u8) u32, input: []const u8) u32 {
+fn find(comptime T: type, comptime size: usize, bank: []const u8) T {
+    std.debug.assert(bank.len >= size);
+    var max = [_]u8{'0'} ** size;
+    var i: usize = 0;
+    while (i < bank.len) : (i += 1) {
+        const joltage = bank[i];
+        var digit = size - @min(bank.len - i, size);
+        while (digit < size) : (digit += 1) {
+            if (joltage > max[digit]) {
+                max[digit] = joltage;
+                @memset(max[digit + 1 ..], '0');
+                break;
+            }
+        }
+    }
+    return std.fmt.parseInt(T, &max, 10) catch unreachable;
+}
+
+test "find(u32, 2, line) finds largest joltage" {
+    try std.testing.expectEqual(98, find(u32, 2, "987654321111111"));
+    try std.testing.expectEqual(89, find(u32, 2, "811111111111119"));
+    try std.testing.expectEqual(78, find(u32, 2, "234234234234278"));
+    try std.testing.expectEqual(92, find(u32, 2, "818181911112111"));
+}
+test "find(u64, 12, line) finds largest joltage" {
+    try std.testing.expectEqual(987654321111, find(u64, 12, "987654321111111"));
+    try std.testing.expectEqual(811111111119, find(u64, 12, "811111111111119"));
+    try std.testing.expectEqual(434234234278, find(u64, 12, "234234234234278"));
+    try std.testing.expectEqual(888911112111, find(u64, 12, "818181911112111"));
+}
+
+fn solve(comptime T: type, comptime size: usize, input: []const u8) T {
     var it = std.mem.splitScalar(u8, input, '\n');
-    var total: u32 = 0;
+    var total: T = 0;
     while (it.next()) |bank| {
-        if (bank.len >= 2) total += find(bank);
+        if (bank.len > 0) total += find(T, size, bank);
     }
     return total;
 }
-
-fn find1(bank: []const u8) u32 {
-    std.debug.assert(bank.len >= 2);
-    var upper = bank[0] - '0';
-    var lower = bank[1] - '0';
-    var index: usize = 1;
-    while (index < bank.len - 1) : (index += 1) {
-        const joltage = bank[index] - '0';
-        if (joltage > upper) {
-            upper = joltage;
-            lower = bank[index + 1] - '0';
-        } else if (joltage > lower) lower = joltage;
-    }
-    return upper * 10 + @max(lower, bank[index] - '0');
+test "solve(u32, 2, example) calculates correct total" {
+    try std.testing.expectEqual(357, solve(u32, 2,
+        \\987654321111111
+        \\811111111111119
+        \\234234234234278
+        \\818181911112111
+    ));
 }
-test "find1(line) finds largest joltage" {
-    try std.testing.expectEqual(98, find1("987654321111111"));
-    try std.testing.expectEqual(89, find1("811111111111119"));
-    try std.testing.expectEqual(78, find1("234234234234278"));
-    try std.testing.expectEqual(92, find1("818181911112111"));
-}
-test "solve(find1, example) calculates correct total" {
-    try std.testing.expectEqual(357, solve(find1,
+test "solve(u64, 12, example) calculates correct total" {
+    try std.testing.expectEqual(3121910778619, solve(u64, 12,
         \\987654321111111
         \\811111111111119
         \\234234234234278
@@ -42,7 +60,8 @@ pub fn main() !void {
     const input = @embedFile("./inputs/3.txt");
 
     var writer = std.fs.File.stdout().writer(&.{});
-    try writer.interface.print("{d}\n", .{
-        solve(find1, input),
+    try writer.interface.print("{d}\n{d}\n", .{
+        solve(u32, 2, input),
+        solve(u64, 12, input),
     });
 }
