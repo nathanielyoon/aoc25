b184cf0b909e05893c3b474978cba1592af52841
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-12-04 15:30:49 -0500

    new: complete 04.2

--- 4.zig
+++ 4.zig
@@ -13,9 +13,7 @@ const example =
 ;
 
 fn measure(comptime diagram: []const u8) comptime_int {
-    for (diagram, 0..) |c, i| {
-        if (c == '\n') return i;
-    }
+    for (diagram, 0..) |c, i| if (c == '\n') return i;
     unreachable;
 }
 test "measure(example) finds right width" {
@@ -33,14 +31,15 @@ fn check(above: []const u8, below: []const u8) u8 {
     return total;
 }
 
-fn count(comptime source: []const u8, target: []u8) void {
-    const width = measure(source);
+fn count(width: comptime_int, source: []const u8, target: []u8) void {
     std.debug.assert(target.len == source.len);
+
     var above = [_]u8{'.'} ** width;
     var it = std.mem.splitScalar(u8, std.mem.trimEnd(u8, source, "\n"), '\n');
     var i: usize = 0;
     while (it.next()) |row| : (i += width + 1) {
         const below = it.peek() orelse &[_]u8{'.'} ** width;
+
         // Check leftmost edge.
         target[i] += check(above[0..2], below[0..2]);
         target[i] += isRoll(row[1]);
@@ -56,13 +55,14 @@ fn count(comptime source: []const u8, target: []u8) void {
         target[i + width - 1] += check(above[width - 2 ..], below[width - 2 ..]);
         target[i + width - 1] += isRoll(row[width - 2]);
 
+        // This row is above the next one.
         @memcpy(&above, row);
     }
 }
 
 fn solve1(comptime source: []const u8) u32 {
     var target = [_]u8{0} ** source.len;
-    count(source, &target);
+    count(measure(source), source, &target);
     var total: u32 = 0;
     for (source, target) |s, t| if (s == '@' and t < 4) {
         total += 1;
@@ -73,10 +73,34 @@ test "solve1(example) solves 1" {
     try std.testing.expectEqual(13, solve1(example));
 }
 
+fn solve2(comptime source: []u8) u32 {
+    var target: [source.len]u8 = undefined;
+
+    const width = measure(source);
+    var total: u32 = 0;
+    // This value gets overwritten on each iteration, it just has to start out
+    // different from `total`.
+    var prev: u32 = 1;
+    while (total != prev) {
+        prev = total;
+        @memset(&target, 0);
+        count(width, source, &target);
+        for (source, target, 0..) |s, t, i| if (s == '@' and t < 4) {
+            total += 1;
+            source[i] = '.';
+        };
+    }
+    return total;
+}
+test "solve2(example) solves 2" {
+    try std.testing.expectEqual(43, solve2(@constCast(example)));
+}
+
 pub fn main() !void {
     const input = @embedFile("./inputs/4.txt");
     var writer = std.fs.File.stdout().writer(&.{}).interface;
-    try writer.print("{d}\n", .{
+    try writer.print("{d}\n{d}\n", .{
         solve1(input),
+        solve2(@constCast(input)),
     });
 }
