0704a20bf888e9a4eeff4c57141858a200b17fb6
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-12-10 15:51:49 -0500

    new: keep on trying 09.2

--- 9.zig
+++ 9.zig
@@ -10,6 +10,11 @@ const example =
     \\2,3
     \\7,3
 ;
+pub fn main() !void {
+    const input = @embedFile("./inputs/9.txt");
+    try lib.print(try solve1(input));
+    try lib.print(try solve2(input));
+}
 
 const Tile = @Vector(2, usize);
 fn parseTiles(allocator: std.mem.Allocator, input: []const u8) ![]Tile {
@@ -75,176 +80,107 @@ test "solve1(example)" {
     try std.testing.expectEqual(50, try solve1(example));
 }
 
-const Color = enum(u2) { red_0, red_1, green, other };
-const Floor = struct {
-    min_rows: usize,
-    min_cols: usize,
-    max_rows: usize,
-    max_cols: usize,
-    colors: []Color,
-    fn init(
-        allocator: std.mem.Allocator,
-        min_rows: usize,
-        min_cols: usize,
-        max_rows: usize,
-        max_cols: usize,
-    ) !Floor {
-        std.debug.print("{d}\n", .{(max_rows - min_rows) * (max_cols - min_rows)});
-        const colors = try allocator.alloc(Color, (max_rows - min_rows) * (max_cols - min_rows));
-        @memset(colors, Color.other);
-        return Floor{
-            .min_rows = min_rows,
-            .min_cols = min_cols,
-            .max_rows = max_rows,
-            .max_cols = max_cols,
-            .colors = colors,
-        };
-    }
-    fn getIndex(self: *Floor, row: usize, col: usize) usize {
-        std.debug.assert(row >= self.min_rows);
-        std.debug.assert(col >= self.min_cols);
-        std.debug.assert(row < self.max_rows);
-        std.debug.assert(col < self.max_cols);
-        return (row - self.min_rows) * (self.max_cols - self.min_cols) + col - self.min_cols;
-    }
-    fn deinit(self: *Floor, allocator: std.mem.Allocator) void {
-        allocator.free(self.colors);
-    }
-    fn set(self: *Floor, row: usize, col: usize, color: Color) void {
-        self.colors[self.getIndex(row, col)] = color;
-    }
-    fn get(self: *Floor, row: usize, col: usize) Color {
-        return self.colors[self.getIndex(row, col)];
-    }
-    fn print(self: *Floor) void {
-        for (self.min_rows..self.max_rows) |row| {
-            for (self.min_cols..self.max_cols) |col| {
-                std.debug.print("{d}", .{self.get(row, col)});
-            }
-            std.debug.print("\n", .{});
-        }
-    }
-};
+const Row = std.array_list.Aligned([2]usize, null);
+fn drawRows(allocator: std.mem.Allocator, tiles: []Tile) ![]Row {
+    var max: usize = 0;
+    for (tiles) |tile| max = @max(tile[0] + 1, max);
 
-fn colorFloor(allocator: std.mem.Allocator, tiles: []Tile) !Floor {
-    // Calculate dimensions of (legal) floor.
-    var min_cols = tiles[0][0];
-    var min_rows = tiles[0][1];
-    var max_cols = tiles[0][0];
-    var max_rows = tiles[0][1];
-    for (tiles[1..]) |tile| {
-        if (tile[0] < min_cols) min_cols = tile[0];
-        if (tile[1] < min_rows) min_rows = tile[1];
-        if (tile[0] >= max_cols) max_cols = tile[0] + 1;
-        if (tile[1] >= max_rows) max_rows = tile[1] + 1;
-    }
+    const rows = try allocator.alloc(Row, max);
+    for (0..max) |row| rows[row] = try Row.initCapacity(allocator, 0);
 
-    var floor = try Floor.init(allocator, min_rows, min_cols, max_rows, max_cols);
     var prev = tiles[tiles.len - 1];
-
     for (tiles) |tile| {
         const row = tile[1];
-        // Set horizontal lines.
         if (row == prev[1]) {
-            const min = @min(tile[0], prev[0]);
-            const max = @max(tile[0], prev[0]);
-            floor.set(row, min, .red_0);
-            floor.set(row, max, .red_1);
-            for (min + 1..max) |col| floor.set(row, col, .green);
+            try rows[row].append(allocator, .{
+                @min(tile[0], prev[0]),
+                @max(tile[0], prev[0]),
+            });
         }
         prev = tile;
     }
 
-    for (min_cols..max_cols) |col| {
-        var is_green = false;
-        var last_red = Color.other;
-        for (min_rows..max_rows) |row| {
-            const color = floor.get(row, col);
-            switch (color) {
-                .other => if (is_green or last_red != .other) floor.set(row, col, .green),
-                .green => is_green = !is_green,
-                .red_0, .red_1 => switch (last_red) {
-                    .red_0, .red_1 => {
-                        is_green = (last_red == color) == is_green;
-                        last_red = .other;
-                    },
-                    .other => last_red = color,
-                    .green => unreachable,
-                },
-            }
-        }
-    }
-
-    return floor;
+    return rows;
 }
-test "colorFloor(example)" {
+test "drawRows(example)" {
     const allocator = std.testing.allocator;
 
     const tiles = try parseTiles(allocator, example);
     defer allocator.free(tiles);
 
-    var floor = try colorFloor(allocator, tiles);
-    defer floor.deinit(allocator);
+    const rows = try drawRows(allocator, tiles);
+    defer {
+        for (rows) |row| @constCast(&row).deinit(allocator);
+        allocator.free(rows);
+    }
 
-    var i: usize = 0;
-    for (
-        \\.....#XXX#
-        \\.....XXXXX
-        \\#XXXX#XXXX
-        \\XXXXXXXXXX
-        \\#XXXXXX#XX
-        \\.......XXX
-        \\.......#X#
-    ) |char| {
-        const color = floor.colors[i];
-        switch (char) {
-            '.' => try std.testing.expectEqual(Color.other, color),
-            '#' => try std.testing.expect(color == Color.red_0 or color == Color.red_1),
-            'X' => try std.testing.expectEqual(Color.green, color),
-            '\n' => continue,
-            else => unreachable,
-        }
-        i += 1;
-    }
-    for (tiles) |tile| {
-        const color = floor.get(tile[1], tile[0]);
-        try std.testing.expect(color == .red_0 or color == .red_1);
-    }
+    try std.testing.expectEqualSlices([2]usize, &.{}, rows[0].items);
+    try std.testing.expectEqualSlices([2]usize, &.{.{ 7, 11 }}, rows[1].items);
+    try std.testing.expectEqualSlices([2]usize, &.{}, rows[2].items);
+    try std.testing.expectEqualSlices([2]usize, &.{.{ 2, 7 }}, rows[3].items);
+    try std.testing.expectEqualSlices([2]usize, &.{}, rows[4].items);
+    try std.testing.expectEqualSlices([2]usize, &.{.{ 2, 9 }}, rows[5].items);
+    try std.testing.expectEqualSlices([2]usize, &.{}, rows[6].items);
+    try std.testing.expectEqualSlices([2]usize, &.{.{ 9, 11 }}, rows[7].items);
 }
 
+const Red = enum { none, min, max };
 fn solve2(input: []const u8) !u64 {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
     const tiles = try parseTiles(allocator, input);
-    var floor = try colorFloor(allocator, tiles);
+    const rows = try drawRows(allocator, tiles);
 
     var max: u64 = 0;
+    // Loop over each tile.
     for (tiles, 0..) |one, i| {
-        mid: for (tiles[i + 1 ..]) |two| {
+        // Loop over each possible combination, except impossible/earlier ones.
+        loop: for (tiles[i + 1 ..]) |two| {
             const area = calculateArea(one, two);
-            if (area < max) continue :mid;
-            const col_0 = @min(one[0], two[0]);
-            const row_0 = @min(one[1], two[1]);
-            const row_1 = @max(one[1], two[1]);
-            const col_1 = @max(one[0], two[0]);
-            for (row_0..row_1 + 1) |row| {
-                for (col_0..col_1 + 1) |col| {
-                    if (floor.get(row, col) == .other) continue :mid;
-                }
+            if (area <= max) continue :loop;
+
+            // This is the furthest-down row. Tiles between the top edge and
+            // here affect the color of this rectangle.
+            const end: usize = @max(one[1], two[1]);
+            // Loop over each column.
+            for (@min(one[0], two[0])..@max(one[0], two[0]) + 1) |col| {
+                // Track whether inside/outside the loop.
+                var is_green = false;
+                // Track the most recently-visited red tile. If there's an
+                // unmatched one, then all subsequent tiles are green until it's
+                // matched. When that happens, the state may or may not change,
+                // depending on whether the two red tiles are on the same end of
+                // their respective lines.
+                var last_red = Red.none;
+                // Loop over each row's lines.
+                for (rows[0..end]) |lines| for (lines.items) |line| {
+                    if (col == line[0]) switch (last_red) {
+                        .none => last_red = .min,
+                        .min => last_red = .none,
+                        .max => {
+                            is_green = !is_green;
+                            last_red = .none;
+                        },
+                    } else if (col == line[1]) switch (last_red) {
+                        .none => last_red = .max,
+                        .min => {
+                            is_green = !is_green;
+                            last_red = .none;
+                        },
+                        .max => last_red = .none,
+                    } else if (col < line[0] and col > line[1]) {
+                        is_green = !is_green;
+                    } else if (!is_green and last_red == .none) continue :loop;
+                };
             }
             max = area;
         }
     }
+
     return max;
 }
 test "solve2(example)" {
     try std.testing.expectEqual(24, try solve2(example));
-}
-
-pub fn main() !void {
-    const input = @embedFile("./inputs/9.txt");
-    try lib.print(try solve1(input));
-    try lib.print(try solve2(input));
 }
